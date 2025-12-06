e134b9c882a5ab9cb40cf3019952a6b8f5a43dce
Manjesh Kumar <manjeshkumarbgs6@gmail.com>
2025-12-06 17:16:46 +0530

    push_data.py

--- push_data.py
+++ push_data.py
@@ -53,8 +53,10 @@ if __name__=='__main__':
     DATABASE="manjesh38"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
+    ## convert data into json format
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
     print(records)
+    ## After converting data into json format insert data into mongodb
     no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
     print(no_of_records)
         
