26eecc0ead49fd3d019cde9c2a145b73ae6ebf99
Manjesh Kumar <manjeshkumarbgs6@gmail.com>
2026-01-13 13:10:38 +0530

    change

--- push_data.py
+++ push_data.py
@@ -9,6 +9,7 @@ MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 print(MONGO_DB_URL)
 
 import certifi
+## trusted certificates ka address deta hai jisse secure connection possible hota hai
 ca=certifi.where()
 
 import pandas as pd
@@ -26,8 +27,11 @@ class NetworkDataExtract():
         
     def csv_to_json_convertor(self,file_path):
         try:
+            ## Read the dataset from source file
             data=pd.read_csv(file_path)
+            ## drop their index column from dataset
             data.reset_index(drop=True,inplace=True)
+            ## return krta hai list of json
             records=list(json.loads(data.T.to_json()).values())
             return records
         except Exception as e:
@@ -38,16 +42,17 @@ class NetworkDataExtract():
             self.database=database
             self.collection=collection
             self.records=records
-
+            ## iss address wale mongodb server se connect ho jao
             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
             self.database = self.mongo_client[self.database]
             
             self.collection=self.database[self.collection]
+            ## yaha pe data json format ko insert kar diya table me approx 11k rows and 31 columns
             self.collection.insert_many(self.records)
             return(len(self.records))
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
+  ## start execution of etl pipeline      
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
     DATABASE="manjesh38"
